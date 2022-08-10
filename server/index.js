2176d742a3372721270554426a016405110a602f
Jay Slade <brandonslade2@gmail.com>
2022-08-09 20:18:19 -0700

    BackEnd1

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/api/users", (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+  });
+
+
+  app.get("/weather/:temperature", (req, res) => {
+    const { temperature } = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+  });
+
+
+
+app.listen(4000, () => console.log("Server running on port 4000"))
+
